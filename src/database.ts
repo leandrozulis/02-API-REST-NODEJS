8655280added9d8589c0650805f62ff1d427fe7e
Leandro <Leandro@leandro.com>
2023-11-28 11:20:45 -0300

    asdf

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
   connection: env.DATABASE_CLIENT === 'sqlite' ? {
     filename: env.DATABASE_URL,
-  } : env.DATABASE_URL,
+  } : env.DATABASE_CLIENT,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
